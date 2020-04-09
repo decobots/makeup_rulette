781dd29c34adb876ac3a143c75e911e374194023
decobots <deco-bots@yandex.ru>
2020-04-09 14:59:13 +0300

    templates added to static

--- rulette/settings.py
+++ rulette/settings.py
@@ -131,6 +131,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'rul/templates'),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
