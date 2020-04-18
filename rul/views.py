f1ce4485f3393d8576ae5f2792939290ea5f68e4
decobots <deco-bots@yandex.ru>
2020-04-18 16:19:05 +0300

    debug panel fixed

--- rul/views.py
+++ rul/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django_registration.forms import User
 
@@ -14,18 +14,28 @@ def index(request):
 
 @login_required
 def random_selector(request):
-    shades = Shade.objects.select_related('palette__seller').all().order_by('?')[:4]
+    palettes_id = users_palettes(request.user.pk)
+    shades = Shade.objects.filter(palette_id__in=palettes_id).select_related('palette__seller').all().order_by('?')[:4]
     context = {'shades_list': shades}
     return render(request, 'rul/random.html', context)
 
 
 @login_required
 def insta_glam_selector(request):
-    crease = Shade.objects.select_related('palette__seller').filter(texture='M', darkness=3).all().order_by('?')[:1]
+    palettes_id = users_palettes(request.user.pk)
+    crease = Shade.objects.select_related('palette__seller').filter(
+        texture='M', darkness=3, palette_id__in=palettes_id).all().order_by('?')[:1]
+
     inner = Shade.objects.select_related('palette__seller').filter(
+        palette_id__in=palettes_id).filter(
         Q(texture='Sh') | Q(texture='Sp') | Q(texture='G') | Q(darkness__gt=3)).all().order_by('?')[:1]
-    outer_v = Shade.objects.select_related('palette__seller').filter(darkness__lt=3).all().order_by('?')[:1]
-    blending = Shade.objects.select_related('palette__seller').filter(darkness__gt=3).all().order_by('?')[:1]
+
+    outer_v = Shade.objects.select_related('palette__seller').filter(
+        palette_id__in=palettes_id, darkness__lt=3).all().order_by('?')[:1]
+
+    blending = Shade.objects.select_related('palette__seller').filter(
+        palette_id__in=palettes_id, darkness__gt=3).all().order_by('?')[:1]
+
     context = {"shades_list": [crease[0], inner[0], outer_v[0], blending[0]]}
     return render(request, 'rul/insta_glam_selector.html', context)
 
@@ -39,8 +49,7 @@ def shade_detail(request, shade_id):
 @login_required
 def rainbow(request):
     colors = {}
-    user_palettes = UserPalette.objects.select_related('palette__seller').filter(user_id=request.user.pk).all()
-    palettes_id = [p.palette.id for p in user_palettes]
+    palettes_id = users_palettes(request.user.pk)
     shades = Shade.objects.select_related('palette__seller').filter(palette_id__in=palettes_id).all()
     for color in Shade.COLORS:
         #
@@ -81,3 +90,8 @@ def user_palette(request):
 def user_palette_saved(request):
     palettes = UserPalette.objects.filter(user_id=request.user.pk).select_related("palette")
     return render(request, 'rul/user_palette_saved.html', {'palettes': palettes})
+
+
+def users_palettes(user_id):
+    user_palettes = UserPalette.objects.select_related('palette__seller').filter(user_id=user_id).all()
+    return [p.palette.id for p in user_palettes]
