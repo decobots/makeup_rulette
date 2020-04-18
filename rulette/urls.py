42e20dcfe5de216a848bcf7766c0c32e9f8f6c91
decobots <deco-bots@yandex.ru>
2020-04-18 16:10:16 +0300

    debug panel fixed

--- rulette/urls.py
+++ rulette/urls.py
@@ -17,11 +17,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-if settings.DEBUG:
-    import debug_toolbar
-urlpatterns = [path('__debug__/', include(debug_toolbar.urls)),
+
+urlpatterns = [
                   path('', include('rul.urls')),
                   path('admin/', admin.site.urls),
                   path('accounts/', include('django_registration.backends.one_step.urls')),
                   path('accounts/', include('django.contrib.auth.urls')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
