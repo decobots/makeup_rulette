ac06b7e873c782cfeda69e031f319811e4625ea6
decobots <deco-bots@yandex.ru>
2020-04-20 14:30:00 +0300

    user-shades added

--- rul/urls.py
+++ rul/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('insta_glam_selector', views.insta_glam_selector, name='insta_glam'),
     path('user_palette', views.user_palette, name='user_palette'),
     path('user_palette_saved', views.user_palette_saved, name='user_palette_saved'),
+    path('user_shade', views.user_shade, name='user_shade'),
 
     # ex: /polls/5/results/
 ]
